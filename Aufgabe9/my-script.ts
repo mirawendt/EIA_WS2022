65eca63f5b906a41b76b192befcce2d925a1b9ed
mirawendt <mira.sasbach@web.de>
2022-12-09 13:02:43 +0100

    remix

--- Aufgabe9/my-script.ts
+++ Aufgabe9/my-script.ts
@@ -1,83 +1,106 @@
 
 namespace Aufgabe9 {
-const sound1: HTMLAudioElement = new Audio('./DrumPad/A.mp3');
-const sound2: HTMLAudioElement = new Audio('./DrumPad/C.mp3');
-const sound3: HTMLAudioElement = new Audio('./DrumPad/F.mp3');
-const sound4: HTMLAudioElement = new Audio('./Drumpad/G.mp3');
-const sound5: HTMLAudioElement = new Audio('./DrumPad/hihat.mp3');
-const sound6: HTMLAudioElement = new Audio('./DrumPad/kick.mp3');
-const sound7: HTMLAudioElement = new Audio('./DrumPad/laugh-1.mp3');
-const sound8: HTMLAudioElement = new Audio('./DrumPad/laugh-2.mp3');
-const sound9: HTMLAudioElement = new Audio('./DrumPad/snare.mp3');
-let intervall;
+    const sound1: HTMLAudioElement = new Audio('./DrumPad/A.mp3');
+    const sound2: HTMLAudioElement = new Audio('./DrumPad/C.mp3');
+    const sound3: HTMLAudioElement = new Audio('./DrumPad/F.mp3');
+    const sound4: HTMLAudioElement = new Audio('./Drumpad/G.mp3');
+    const sound5: HTMLAudioElement = new Audio('./DrumPad/hihat.mp3');
+    const sound6: HTMLAudioElement = new Audio('./DrumPad/kick.mp3');
+    const sound7: HTMLAudioElement = new Audio('./DrumPad/laugh-1.mp3');
+    const sound8: HTMLAudioElement = new Audio('./DrumPad/laugh-2.mp3');
+    const sound9: HTMLAudioElement = new Audio('./DrumPad/snare.mp3');
+    let intervall;
 
-function playSample(sound) {
+    function playSample(sound) {
 
-    sound.play()
-}
+        sound.play()
+    }
 
-document.querySelector(".pad-1").addEventListener("click", function () {
-    playSample(sound1)
-})
-document.querySelector(".pad-2").addEventListener("click", function () {
-    playSample(sound2)
-})
-document.querySelector(".pad-3").addEventListener("click", function () {
-    playSample(sound3)
-})
-document.querySelector(".pad-4").addEventListener("click", function () {
-    playSample(sound4)
-})
-document.querySelector(".pad-5").addEventListener("click", function () {
-    playSample(sound5)
-})
-document.querySelector(".pad-6").addEventListener("click", function () {
-    playSample(sound6)
-})
-document.querySelector(".pad-7").addEventListener("click", function () {
-    playSample(sound7)
-})
-document.querySelector(".pad-8").addEventListener("click", function () {
-    playSample(sound8)
-})
-document.querySelector(".pad-9").addEventListener("click", function () {
-    playSample(sound9)
-})
-
-
-
-var beat: string[] = [("./DrumPad/kick.mp3"), ("./DrumPad/hihat.mp3"), ("./DrumPad/snare.mp3")]
-
-var zaehler: number = 0;
+    document.querySelector(".pad-1").addEventListener("click", function () {
+        playSample(sound1)
+    })
+    document.querySelector(".pad-2").addEventListener("click", function () {
+        playSample(sound2)
+    })
+    document.querySelector(".pad-3").addEventListener("click", function () {
+        playSample(sound3)
+    })
+    document.querySelector(".pad-4").addEventListener("click", function () {
+        playSample(sound4)
+    })
+    document.querySelector(".pad-5").addEventListener("click", function () {
+        playSample(sound5)
+    })
+    document.querySelector(".pad-6").addEventListener("click", function () {
+        playSample(sound6)
+    })
+    document.querySelector(".pad-7").addEventListener("click", function () {
+        playSample(sound7)
+    })
+    document.querySelector(".pad-8").addEventListener("click", function () {
+        playSample(sound8)
+    })
+    document.querySelector(".pad-9").addEventListener("click", function () {
+        playSample(sound9)
+    })
 
 
-document.querySelector(".fa-play").addEventListener("click", function () {
-    playBeat()
-    document.querySelector('.fa-stop').classList.remove('hidden')
-    document.querySelector('.fa-play').classList.add('hidden')
-})
 
-document.querySelector(".fa-stop").addEventListener("click", function () {
-    document.querySelector('.fa-play').classList.remove('hidden')
-    document.querySelector('.fa-stop').classList.add('hidden')
-    clearInterval (intervall)
-})
+    var beat: string[] = [("./DrumPad/kick.mp3"), ("./DrumPad/hihat.mp3"), ("./DrumPad/snare.mp3")]
 
-function playThis(soundQuelle: string): void {
-    const sound: HTMLAudioElement = new Audio(soundQuelle);
-    sound.play()
-}
+    var zaehler: number = 0;
 
-function playBeat(): void {
-    intervall=setInterval(function (): void {
-        playThis(beat[zaehler]);
-        console.log("test");
-        zaehler++;
-        if (zaehler === 3) {
-            zaehler = 0;
+
+    document.querySelector(".fa-play").addEventListener("click", function () {
+        playBeat()
+        document.querySelector('.fa-stop').classList.remove('hidden')
+        document.querySelector('.fa-play').classList.add('hidden')
+    })
+
+    document.querySelector(".fa-stop").addEventListener("click", function () {
+        document.querySelector('.fa-play').classList.remove('hidden')
+        document.querySelector('.fa-stop').classList.add('hidden')
+        clearInterval(intervall)
+    })
+
+    function playThis(soundQuelle: string): void {
+        const sound: HTMLAudioElement = new Audio(soundQuelle);
+        sound.play()
+    }
+
+    function playBeat(): void {
+        intervall = setInterval(function (): void {
+            playThis(beat[zaehler]);
+            console.log("test");
+            zaehler++;
+            if (zaehler === 3) {
+                zaehler = 0;
+            }
+        }, 500);
+    }
+
+
+
+    
+    let myArray = [(sound5), (sound6), (sound9)]
+
+    document.querySelector(".fa-microphone").addEventListener("click", function () {
+
+        for (var i = 0; i < 4; i++) {
+            const number = Math.random;
+            myArray - push(number);
         }
-    }, 500);
+
+    })
+
+
+
 }
+
+
+
+
+
 
 
 
@@ -114,4 +137,3 @@ function playBeat(): void {
 
 //classList.length
 //gibt die Zahl der Klassen in der classList an
-}
